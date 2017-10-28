5f79e26794aeda7ef62ee9e4ccaaebbce0466682
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-10-29 00:48:56 +0200

    Searching

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,69 @@
 import * as React from 'react'
-import ListShelves from './Components/ListShelves'
 import { Route, Link } from 'react-router-dom'
+import { debounce } from 'lodash'
+import Shelf from './Components/Shelf'
+import ListShelves from './Components/ListShelves'
+import * as BooksAPI from './ext/BooksAPI'
+import { BookInterface } from './Components/Book'
 import './style/App.css'
 
 class BooksApp extends React.Component {
+  state: {
+    loadingBooks: boolean
+    searchingBooks: boolean
+    books: BookInterface[]
+    bookSearchResult: BookInterface[]
+    query: string
+    lastQuery: string | null
+  } = {
+    loadingBooks: false,
+    searchingBooks: false,
+    books: [],
+    bookSearchResult: [],
+    query: '',
+    lastQuery: null
+  }
+  constructor() {
+    super()
+    this.retrieveBookSearchResults = debounce(this.retrieveBookSearchResults, 500)
+  }
+  componentDidMount() {
+    this.setState({ loadingBooks: true })
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+      this.setState({ loadingBooks: false })
+    })
+  }
+  moveBook = (book: BookInterface, event: React.ChangeEvent<HTMLSelectElement>) => {
+    let books = this.state.books.slice()
+    book.shelf = event.target.value
+    let thisBook = books.find(b => b.id === book.id)
+    if (!thisBook) {
+      books.push(book)
+    }
+    this.setState({ books: books })
+    BooksAPI.update(book, event.target.value)
+  }
+
+  searchBooks = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ query: event.target.value })
+    // Note: this is debounced, see constructor
+    this.retrieveBookSearchResults(event.target.value)
+  }
+
+  retrieveBookSearchResults(query: string) {
+    if ( !query || query === this.state.lastQuery) {
+      return
+    }
+    this.setState({ searchingBooks: true })
+    BooksAPI.search(query).then((result: BookInterface[] & { error?: string }) => {
+      this.setState({
+        bookSearchResult: result && !result.error ? result : [],
+        searchingBooks: false,
+        lastQuery: query
+      })
+    })
+  }
 
   render() {
     return (
@@ -15,11 +75,38 @@ class BooksApp extends React.Component {
               <div className="search-books-bar">
                 <Link to="/" className="close-search">Close</Link>
                 <div className="search-books-input-wrapper">
-                  <input type="text" placeholder="Search by title or author" />
+                  <input
+                    type="text"
+                    placeholder="Search by title or author"
+                    value={this.state.query}
+                    onChange={this.searchBooks}
+                  />
                 </div>
               </div>
               <div className="search-books-results">
-                <ol className="books-grid" />
+                {this.state.searchingBooks ? (
+                  <div className="center">
+                    <div className="loader" />
+                    Search in progress...
+                    </div>
+                ) :
+                  this.state.query &&
+                  this.state.query === this.state.lastQuery &&
+                  this.state.bookSearchResult.length === 0 ? (
+                    <p className="center">No result to show. You might want to search for something else.</p>
+                  ) : !this.state.query && (
+                    <p className="center">Write your searchquery above.</p>
+                  )
+                }
+                {!this.state.searchingBooks && (
+                  <ol className="books-grid" >
+                    <Shelf
+                      books={this.state.bookSearchResult}
+                      shelfName={`Search result for '${this.state.lastQuery}' (${this.state.bookSearchResult.length})`}
+                      onMoveBook={this.moveBook}
+                    />
+                  </ol>
+                )}
               </div>
             </div>
           )}
@@ -32,7 +119,11 @@ class BooksApp extends React.Component {
               <div className="list-books-title">
                 <h1>MyReads</h1>
               </div>
-              <ListShelves/>
+              <ListShelves
+                books={this.state.books}
+                loadingBooks={this.state.loadingBooks}
+                onMoveBook={this.moveBook}
+              />
               <div className="open-search">
                 <Link to="search">Add book</Link>
               </div>
--- src/Components/ListShelves.tsx
+++ src/Components/ListShelves.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Shelf from './Shelf'
 import { BookInterface } from './Book'
-import * as BooksAPI from '../ext/BooksAPI'
 
 export interface BookShelf {
   shelf: 'none' | 'wantToRead' | 'currentlyReading' | 'read' | undefined
@@ -26,52 +25,32 @@ export var bookShelves: BookShelf[] = [
     shelfText: 'Previously read'
   }
 ]
-class ListShelves extends React.Component {
-  state: {
-    loadingBooks: boolean
-    books: BookInterface[]
-  } = {
-    loadingBooks: false,
-    books: [],
-  }
-  componentDidMount() {
-    this.setState({ loadingBooks: true })
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
-      this.setState({ loadingBooks: false })
-    })
-  }
-  moveBook = (book: BookInterface, event: React.ChangeEvent<HTMLSelectElement>) => {
-    let books = this.state.books.slice()
-    let thisBook = books.find(b => b.id === book.id)
-    if (!thisBook) {
-      return
-    }
-    thisBook.shelf = event.target.value
-    this.setState({ books: books })
-    BooksAPI.update(thisBook, event.target.value)
-  }
+class ListShelves extends React.Component<{
+  books: BookInterface[]
+  loadingBooks: boolean
+  onMoveBook: (book: BookInterface, event: React.ChangeEvent<HTMLSelectElement>) => void
+}> {
   render() {
     return (
       <div className="list-books-content">
-        {this.state.loadingBooks ? (
+        {this.props.loadingBooks ? (
           <div className="center">
             <div className="loader" />
             Gathering your books...
                       </div>
-        ) : this.state.books.length === 0 && (
+        ) : this.props.books.length === 0 && (
           <p className="center">
             No books to see here.
-                      Maybe you should add a few.
-                      I promise, it won't actually hurt your brain.</p>
+            Maybe you should add a few.
+            I promise, it won't actually hurt your brain.</p>
         )}
         {bookShelves.map(shelf => (
           <Shelf
             key={shelf.shelf}
-            books={this.state.books.filter(
+            books={this.props.books.filter(
               book => book.shelf === shelf.shelf)}
             shelfName={shelf.shelfText}
-            onMoveBook={this.moveBook}
+            onMoveBook={this.props.onMoveBook}
           />
         ))}
       </div>
